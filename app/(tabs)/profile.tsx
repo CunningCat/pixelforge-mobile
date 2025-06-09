fd138e124664b36ca2847d196f51e7221ade42b3
rabbit <cunning_rabbit@163.com>
2025-06-09 17:22:16 +0800

    初始化框架

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function profile() {
+  return (
+    <SafeAreaView>
+      <Text className="dark:text-white">Profile</Text>
+      
+    </SafeAreaView>
+  );
+}
